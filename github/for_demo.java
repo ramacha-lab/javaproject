8c67b6bfde68a26795f87ded7145e5d2421e0d2e
chandra <rns.agrahara@gmail.com>
2025-04-08 15:19:43 +0530

    Add files via upload

---
+++ github/for_demo.java
@@ -0,0 +1,13 @@
+import java .io. DataInputStream;
+class factorial_demo
+{
+	public static void main (String args [])
+	{
+             	DataInputStream n=new DataInputStream(System.in);
+		System.out.println("enter the value for s");
+		s=Integer.praseInt(n.readLine());
+		int fact=1;
+		try
+		{
+			
+		for (i=1;i<=S;i+=)
